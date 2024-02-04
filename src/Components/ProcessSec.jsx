cbfe6b17b1167a36578aed5b7f6b8474785a977f
mashrik <mashrikmohammed10@gmail.com>
2024-02-04 11:17:46 +0530

    height problem fixed images compressed

--- src/Components/ProcessSec.jsx
+++ src/Components/ProcessSec.jsx
@@ -5,7 +5,7 @@ function ProcessSec() {
     <div id="about" className="pb-[50px] pt-[20px] md:pt-[90px]  ">
       <div className="textdiv">
         <h1 className="text-[35px] sm:text-[48px] font-[700] leading-[45px]">
-          Behind the Lens:Our Process
+          Behind the Lens: Our Process
         </h1>
         <p className=" text-[20px] sm:text-[24px] font-[500] text-gray-600 mt-[10px]">
           Discover How We Create Stunning Photos You'll Love
@@ -14,7 +14,7 @@ function ProcessSec() {
 
       <div>
         <div className=" md:flex text-white mt-[50px] justify-between">
-          <div className="adiv w-[30%] h-[300px]  pt-[20px] px-[20px] pb-[50px]">
+          <div className="adiv w-[30%] h-[370px]  pt-[20px] px-[20px] pb-[50px]">
             <h1 className="text-[23px] font-semibold">
               Unforgettable Memories
             </h1>
@@ -27,7 +27,7 @@ function ProcessSec() {
               gallery of stunning stories that speak for themselves.
             </div>
           </div>
-          <div className="bdiv w-[30%]  h-[300px]  pt-[20px] px-[20px] pb-[50px]">
+          <div className="bdiv w-[30%]  h-[370px]  pt-[20px] px-[20px] pb-[50px]">
             <h1 className="text-[23px] font-semibold">Affordable price</h1>
             <p className="text-[18px]">Customized Just For You</p>
             <div className=" leading-[20px] text-[14px] sm:text-[16px]  mt-[40px]">
@@ -38,7 +38,7 @@ function ProcessSec() {
               dream wedding.
             </div>
           </div>
-          <div className="cdiv w-[30%]  h-[300px]  pt-[20px] px-[20px] pb-[50px]">
+          <div className="cdiv w-[30%] h-[370px]  pt-[20px] px-[20px] pb-[50px]">
             <h1 className="text-[23px] font-semibold">
               Picture-Perfect Moments
             </h1>
@@ -56,39 +56,40 @@ function ProcessSec() {
           </div>
         </div>
         <div className=" col md:flex text-white mt-[20px] justify-between">
-          <div className="ddiv w-[30%]  h-[300px]  pt-[20px] px-[20px] pb-[50px]">
+          <div className="ddiv w-[30%]  h-[370px]  pt-[20px] px-[20px] pb-[50px]">
             <h1 className="text-[23px] font-semibold">Customized Albums </h1>
             <p className="text-[18px]">From Dreams to Reality</p>
             <div className=" leading-[20px] text-[14px] sm:text-[16px] mt-[40px]">
-              At Moodwedlock, we use the latest gadgets and machines to create
-              bespoke packages that fit your budget. Our advanced products help
-              us capture the unique essence of your special day while offering
-              prices that won't break the bank. Contact us now to discuss your
-              dream wedding.
+              Transform your dreams into tangible memories with our personalized
+              album service. From cherished moments to extraordinary milestones,
+              we craft bespoke albums that bring your visions to life, ensuring
+              every page tells your unique story with a touch of artistry and
+              sentimentality.
             </div>
           </div>
-          <div className="ediv w-[30%]  h-[300px]  pt-[20px] px-[20px] pb-[50px]">
+          <div className="ediv w-[30%] h-[370px]  pt-[20px] px-[20px] pb-[50px]">
             <h1 className="text-[23px] font-semibold">Our Dedicated Team</h1>
             <p className="text-[18px]">Unrelenting, Efficient, and Reliable</p>
             <div className=" leading-[20px] text-[14px] sm:text-[16px]  mt-[40px]">
-              At Moodwedlock, we use the latest gadgets and machines to create
-              bespoke packages that fit your budget. Our advanced products help
-              us capture the unique essence of your special day while offering
-              prices that won't break the bank. Contact us now to discuss your
-              dream wedding.
+              Our dedicated team embodies the principles of Unrelenting
+              commitment, Efficient execution, and Reliable service. With
+              unwavering dedication, we tirelessly strive to meet and exceed
+              your expectations. Our efficiency ensures timely and precise
+              results, while our reliability guarantees a trustworthy
+              partnership you can count on for every endeavor.
             </div>
           </div>
-          <div className="fdiv w-[30%]  h-[300px]  pt-[20px] px-[20px] pb-[50px]">
+          <div className="fdiv w-[30%]  h-[370px]  pt-[20px] px-[20px] pb-[50px]">
             <h1 className="text-[23px] font-semibold">
               Timely Delivery Guaranteed
             </h1>
             <p className="text-[18px]">Efficient Post-Processing</p>
             <div className=" leading-[20px] text-[14px] sm:text-[16px]  mt-[40px]">
-              Transform your special day into magical stories that you'll
-              cherish for a lifetime. Our team of experts and technicians work
-              tirelessly behind the scenes to provide services that are truly
-              unforgettable. Don't just take our word for it - check out our
-              gallery of stunning stories that speak for themselves.
+              Experience the assurance of timely delivery with our guaranteed
+              efficient post-processing services. We prioritize swift and
+              precise handling of your needs, ensuring that the final results
+              not only meet but exceed your expectations. Trust us for reliable,
+              punctual, and high-quality post-processing solutions.
             </div>
           </div>
         </div>
