d140b156d507e98d4025b20e61b88e6ceef7cf2d
mashrik <mashrikmohammed10@gmail.com>
2024-08-04 16:06:38 +0530

    images changed

--- src/Components/Herosection.jsx
+++ src/Components/Herosection.jsx
@@ -9,11 +9,11 @@ function HeroSection() {
       <div className="text-center headdiv flex flex-col justify-center items-center h-[100vh] px-[10px] ">
         {" "}
         <h1 className="text-white text-[33px]  xs:text-[35px] leading-[40px] xs:leading-[50px] sm:leading-[70px] sm:text-[58px]   font-semibold">
-          Capture Your Best Moment with Us.
+          Acknowledge the elegance of simplicity
         </h1>
-        <p className="text-gray-300 text-[18px] sm:text-[24px] tracking-[1px] sm:tracking-[2px] ">
+        {/* <p className="text-gray-300 text-[18px] sm:text-[24px] tracking-[1px] sm:tracking-[2px] ">
           High-Quality Images That Tell Your Story
-        </p>
+        </p> */}
       </div>
       <div>
         <p className="text-white text-[10px]  -mt-[100px] sm:text-[15px]">
