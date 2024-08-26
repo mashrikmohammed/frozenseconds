4021b1450085fc943b3f8c0665bbfa8c16f9d661
rishadpt <rishadpt16@gmail.com>
2024-08-26 10:16:43 +0530

    featdone

--- src/Components/Aboutsection.jsx
+++ src/Components/Aboutsection.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 function AboutSection() {
   return (
-    <div id="about"  className=" aboutdiv col md:flex justify-between mt-[60px] items-center">
+    <div
+      id="about"
+      className=" aboutdiv col md:flex justify-between mt-[60px] items-center"
+    >
       <div className="flex picdiv w-fit  ">
         <div className="">
           <div className="pic1 w-[130px] xs:w-[150px] sm:w-[200px] md:w-[250px] h-[200px] sm:h-[250px] md:h-[400px]"></div>
@@ -16,26 +19,22 @@ function AboutSection() {
       <div className="w-fit md:w-[50%] ">
         <div className="textdiv">
           {" "}
-          <h1 className="text-[35px] sm:text-[48px] font-[700]">About</h1>
-          <p className="text-[20px] sm:text-[24px] font-[500] text-gray-600 -mt-[8px]">
+          <h1 className="text-[35px] sm:text-[48px] font-[700]">About us</h1>
+          {/* <p className="text-[20px] sm:text-[24px] font-[500] text-gray-600 -mt-[8px]">
             Our Photography
-          </p>
+          </p> */}
         </div>
 
         <br />
         <p className="text-justify text-gray-700 m-auto text-[14px] sm:text-[16px]">
-          At Frozen Seconds, we believe that photography is a powerful medium
-          for capturing life's most precious moments. Our team of experienced
-          photographers is passionate about creating beautiful, timeless images
-          that tell your unique story. Whether you're looking for wedding
-          photography, family portraits, or commercial shoots, we're here to
-          help you freeze those special moments in time. <br /> <br /> We pride
-          ourselves on our creativity, attention to detail, and commitment to
-          excellence. We take the time to understand your vision and work
-          closely with you to bring it to life. Our goal is to deliver stunning
-          images that you'll cherish for a lifetime. We use only the highest
-          quality equipment and software to ensure that your images are of the
-          highest quality possible.
+          Praanatantra is a celebration of the elegance found in simplicity. Our
+          designs focus on timeless minimalism, blending style, comfort, and
+          versatility in every piece. We believe that true beauty lies in
+          clarity and understated sophistication. <br/><br/>Dedicated to quality and
+          sustainability, we craft our garments with care and respect for
+          ethical practices. At Praanatantra, each creation is a testament to
+          our commitment to offering refined, enduring fashion that transcends
+          trends.
         </p>
       </div>
     </div>
